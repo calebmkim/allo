a5cd68942d6f9ae97b4fc72eb3af3f81e18d97a0
calebmkim <calebkim707@gmail.com>
2024-03-18 10:23:17 -0400

    does this work

--- examples/polybench_hls/adi.py
+++ examples/polybench_hls/adi.py
@@ -85,7 +85,7 @@ def adi(ttype, TSTEPS, N):
                 p[i, 0] = 0
                 q[i, 0] = v[0, i]
                 for j in range(1, N - 1):
-                    p[i, j] = (int) (-c / (a * p[i, j - 1] + b))
+                    p[i, j] = (int) (-c / ((int) (a * p[i, j - 1] + b)))
                     q[i, j] = (int) ((
                         -d * u[j, i - 1]
                         + (1 + 2 * d) * u[j, i]

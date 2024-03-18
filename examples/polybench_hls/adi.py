9703cf36d3b6475597296ecc4f6dfd019dac74b3
calebmkim <calebkim707@gmail.com>
2024-03-18 16:24:43 -0400

    add intermediate value

--- examples/polybench_hls/adi.py
+++ examples/polybench_hls/adi.py
@@ -86,9 +86,10 @@ def adi(ttype, TSTEPS, N):
                 q[i, 0] = v[0, i]
                 for j in range(1, N - 1):
                     p[i, j] = (-c / ((a * p[i, j - 1] + b)))
+                    v = -d * u[j, i - 1]
                     q[i, j] = (
-                        # -d * u[j, i - 1]
-                        (1 + (2 * d)) * u[j, i]
+                        v
+                        + (1 + (2 * d)) * u[j, i]
                         - (f * u[j, i + 1]
                         - a * q[i, j - 1]
                     ) / (a * p[i, j - 1] + b))

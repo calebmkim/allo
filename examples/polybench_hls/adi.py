86170bb4edce271573c2f1351790e0309b372c36
calebmkim <calebkim707@gmail.com>
2024-03-18 16:54:06 -0400

    more casts to int

--- examples/polybench_hls/adi.py
+++ examples/polybench_hls/adi.py
@@ -85,34 +85,34 @@ def adi(ttype, TSTEPS, N):
                 p[i, 0] = 0.0
                 q[i, 0] = v[0, i]
                 for j in range(1, N - 1):
-                    p[i, j] = -c / (a * p[i, j - 1] + b)
-                    q[i, j] = (
+                    p[i, j] = int(-c / (a * p[i, j - 1] + b))
+                    q[i, j] = int((
                         -d * u[j, i - 1]
                         + (1.0 + 2.0 * d) * u[j, i]
                         - f * u[j, i + 1]
                         - a * q[i, j - 1]
-                    ) / (a * p[i, j - 1] + b)
+                    ) / (a * p[i, j - 1] + b))
 
-                v[N - 1, i] = 1.0
+                v[N - 1, i] = int(1.0)
                 for j_rev in range(N - 1):
                     j: index = N - 2 - j_rev
-                    v[j, i] = p[i, j] * v[j + 1, i] + q[i, j]
+                    v[j, i] = int(p[i, j] * v[j + 1, i] + q[i, j])
             for i in range(1, N - 1):
                 u[i, 0] = 1.0
                 p[i, 0] = 0.0
                 q[i, 0] = u[i, 0]
                 for j in range(1, N - 1):
-                    p[i, j] = -f / (d * p[i, j - 1] + e)
-                    q[i, j] = (
+                    p[i, j] = int(-f / (d * p[i, j - 1] + e))
+                    q[i, j] = int((
                         -a * v[i - 1, j]
                         + (1.0 + 2.0 * a) * v[i, j]
                         - c * v[i + 1, j]
                         - d * q[i, j - 1]
-                    ) / (d * p[i, j - 1] + e)
+                    ) / (d * p[i, j - 1] + e))
                 u[i, N - 1] = 1.0
                 for j_rev in range(N - 1):
                     j: index = N - 2 - j_rev
-                    u[i, j] = p[i, j] * u[i, j + 1] + q[i, j]
+                    u[i, j] = int(p[i, j] * u[i, j + 1] + q[i, j])
 
     s = allo.customize(kernel_adi, instantiate=[ttype, TSTEPS, N])
     mod = s.build()

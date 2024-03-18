b9c9bdc5bc3da6e4feac071ca810cdbe0e098431
calebmkim <calebkim707@gmail.com>
2024-03-18 16:16:52 -0400

    comment division

--- examples/polybench_hls/adi.py
+++ examples/polybench_hls/adi.py
@@ -86,12 +86,12 @@ def adi(ttype, TSTEPS, N):
                 q[i, 0] = v[0, i]
                 for j in range(1, N - 1):
                     p[i, j] = (-c / ((a * p[i, j - 1] + b)))
-                    q[i, j] = ((
-                        -d * u[j, i - 1]
-                        + (1 + 2 * d) * u[j, i]
-                        - f * u[j, i + 1]
-                        - a * q[i, j - 1]
-                    ) / (a * p[i, j - 1] + b))
+                    # q[i, j] = ((
+                    #     -d * u[j, i - 1]
+                    #     + (1 + 2 * d) * u[j, i]
+                    #     - f * u[j, i + 1]
+                    #     - a * q[i, j - 1]
+                    # ) / (a * p[i, j - 1] + b))
 
                 v[N - 1, i] = 1.0
                 for j_rev in range(N - 1):

9125a2a2c74990713e20cd7957d41e3224c9733c
calebmkim <calebkim707@gmail.com>
2024-03-20 10:14:26 -0400

    trisolv init

--- examples/polybench_hls/trisolv.py
+++ examples/polybench_hls/trisolv.py
@@ -43,8 +43,8 @@ def test_trisolv():
 
     # generate input data
     N = psize["trisolv"][test_psize]["N"]
-    L = np.random.randint(10, size=(N, N))
-    b = np.random.randint(10, size=(N,))
+    L = np.random.randint(1, 10, size=(N, N))
+    b = np.random.randint(1, 10, size=(N,))
 
     # run reference
     x_ref = np.zeros_like(b)

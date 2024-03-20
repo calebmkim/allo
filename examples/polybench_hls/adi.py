869108fc12404653022a40b8669febb6fcdbaee3
calebmkim <calebkim707@gmail.com>
2024-03-20 10:39:58 -0400

    remove decimals'

--- examples/polybench_hls/adi.py
+++ examples/polybench_hls/adi.py
@@ -83,14 +83,14 @@ def adi(ttype, TSTEPS, N):
     ](u: "T[N, N]", v: "T[N, N]", p: "T[N, N]", q: "T[N, N]"):
         for t in range(1, TSTEPS + 1):
             for i in range(1, N - 1):
-                v[0, i] = 1.0
-                p[i, 0] = 0.0
+                v[0, i] = 1
+                p[i, 0] = 0
                 q[i, 0] = v[0, i]
                 for j in range(1, N - 1):
                     p[i, j] = int(-c / (a * p[i, j - 1] + b))
                     q[i, j] = int((
                         -d * u[j, i - 1]
-                        + (1.0 + 2.0 * d) * u[j, i]
+                        + (1 + 2 * d) * u[j, i]
                         - f * u[j, i + 1]
                         - a * q[i, j - 1]
                     ) / (a * p[i, j - 1] + b))
@@ -100,18 +100,18 @@ def adi(ttype, TSTEPS, N):
                     j: index = N - 2 - j_rev
                     v[j, i] = int(p[i, j] * v[j + 1, i] + q[i, j])
             for i in range(1, N - 1):
-                u[i, 0] = 1.0
-                p[i, 0] = 0.0
+                u[i, 0] = 1
+                p[i, 0] = 0
                 q[i, 0] = u[i, 0]
                 for j in range(1, N - 1):
                     p[i, j] = int(-f / (d * p[i, j - 1] + e))
                     q[i, j] = int((
                         -a * v[i - 1, j]
-                        + (1.0 + 2.0 * a) * v[i, j]
+                        + (1 + 2 * a) * v[i, j]
                         - c * v[i + 1, j]
                         - d * q[i, j - 1]
                     ) / (d * p[i, j - 1] + e))
-                u[i, N - 1] = 1.0
+                u[i, N - 1] = 1
                 for j_rev in range(N - 1):
                     j: index = N - 2 - j_rev
                     u[i, j] = int(p[i, j] * u[i, j + 1] + q[i, j])

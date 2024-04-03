1388603c636eae615de4b7803e7928cb9da926d4
calebmkim <calebkim707@gmail.com>
2024-04-03 10:38:53 -0400

    split computation into separate function

--- tests/test_systolic_array.py
+++ tests/test_systolic_array.py
@@ -122,13 +122,18 @@ def test_subview_systolic_stream():
             for n in range(N):
                 B_drain[n] = B_fifo[n, M, k]
 
+    s0 = allo.customize(systolic_computation)
+    pe = s0.unfold("PE", [0, 1])  # specify which are spatial loops
+    s0.partition(s.C, dim=0)  # required, otherwise it will fail dataflow checking
+    s0.to(s0.A_fifo, pe, axis=1, depth=M + 1)
+    s0.to(s0.B_fifo, pe, axis=0, depth=N + 1)
+
     s = allo.customize(systolic_array)
-    s.partition(s.C, dim=0)  # required, otherwise it will fail dataflow checking
     s.partition(s.A, dim=1)
     s.partition(s.B, dim=2)
-    pe = s.unfold("PE", [0, 1])  # specify which are spatial loops
-    s.to(s.A_fifo, pe, axis=1, depth=M + 1)
-    s.to(s.B_fifo, pe, axis=0, depth=N + 1)
+
+    s.compose(s0)
+
     code = s.build("vhls")
     print(code)
     assert "#pragma HLS dataflow" in str(code)
